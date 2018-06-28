5c902ebb8d6b8ceb9d653a0166a61d72ca433d9b
Rishabhltfb <40674238+Rishabhltfb@users.noreply.github.com>
2018-06-29 01:33:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function showPass(form){
+var name=form.name.value;
+var pass=form.pwd.value;
+var name1="Prakhar"
+var pass1="Bhai"
+var name2="Rishabh"
+var pass2="Hello"
+var name3="Shivani"
+var pass3="Idiot"
+var name4="Deeva"
+var pass4="Pizza"
+var name5="Vidhi"
+var pass5="Smile"
+
+
+if(name==name1 && pass==pass1){
+window.open("Prakhar.html");
+}
+else if(name==name2 && pass==pass2){
+window.open("new.html");
+}
+else if(name==name3 && pass==pass3){
+window.open("Shivani.html");
+}
+else if(name==name4 && pass==pass4){
+window.open("Deeva.html");
+}
+else if(name==name5 && pass==pass5){
+window.open("Vidhi.html");
+}
+else{
+window.open("first.html");
+}
+}
